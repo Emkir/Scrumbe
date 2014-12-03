73d80599586076430ab2e024ea3b4901a573439f
Nirnae <nicofoub@hotmail.fr>
2014-12-03 19:29:58 +0000

    create ProjectBundle

---
+++ src/Scrumbe/Bundle/FrontOfficeBundle/ScrumbeFrontOfficeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Scrumbe\Bundle\FrontOfficeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ScrumbeFrontOfficeBundle extends Bundle
+{
+}
---
+++ src/Scrumbe/Bundle/ProjectBundle/ScrumbeProjectBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Scrumbe\Bundle\ProjectBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ScrumbeProjectBundle extends Bundle
+{
+}
