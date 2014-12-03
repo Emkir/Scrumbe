fb207ce498d93c16e2c86fb052941fec7b922823
Nirnae <nicofoub@hotmail.fr>
2014-12-03 19:48:50 +0000

    Create ProjectBundle Controllers

---
+++ src/Scrumbe/Bundle/ProjectBundle/Controller/TaskController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Scrumbe\Bundle\ProjectBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class TaskController extends Controller
+{
+}
---
+++ src/Scrumbe/Bundle/ProjectBundle/Tests/Controller/UserStoryControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Scrumbe\Bundle\ProjectBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserStoryControllerTest extends WebTestCase
+{
+}
